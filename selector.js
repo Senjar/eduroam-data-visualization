e1b8158c858fe5d6642fe8caf1ec61465a3cdd96
Nikiforos <flywonk@gmail.com>
2020-02-09 16:46:19 +0200

    small fix in selector

--- selector.js
+++ selector.js
@@ -3,17 +3,17 @@ var mapGlobal = document.getElementById('mapGlobal');
 var mapEurope = document.getElementById('mapEurope');
 
 mapGlobal.onclick = function(){
-
+	var old = document.getElementById("container").childNodes[0];
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "chart-global.js."; 
-    document.getElementById("container").appendChild(script);
+    script.src = "chart-global.js"; 
+	old.replaceChild(script, old.childNodes[0]);
 }
 
 mapEurope.onclick = function(){
-
+	var old = document.getElementById("container").childNodes[0];
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "chart-europe.js."; 
-    document.getElementById("container").appendChild(script);
+    script.src = "chart-europe.js"; 
+	old.replaceChild(script, old.childNodes[0]);
 }
